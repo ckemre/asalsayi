ce52ee64adecbd037eb39b7477d7c6d9e97b5339
ckemre <116115682+ckemre@users.noreply.github.com>
2023-02-22 23:07:25 +0300

    Add files via upload

---
+++ asal sayi/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Asal Sayılar :");
+        int i;
+        int j;
+
+        for (i=2;i<=100;i++) {
+            int counter=0;
+
+            for (j=1;j<=i;j++) {
+                if (i%j ==0) {
+                    counter++;
+                }
+            }   if (counter <=2) {
+                System.out.println(i);
+            }
+        }
+    }
+}
